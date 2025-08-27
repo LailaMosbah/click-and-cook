84d6c05f0c1213ca70f22fb15348b2495a0023f7
LailaMosbah <lylymsbah2@gmail.com>
2025-08-27 22:19:27 +0300

    try to deply

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter
-    basename={import.meta.env.MODE === "development" ? "/" : "/click-and-cook"}
-  >
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter
+      basename={import.meta.env.MODE === "development" ? "" : "/click-and-cook"}
+    >
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
