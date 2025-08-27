77d81fdeb0449869347419c71fc352f65066560e
LailaMosbah <lylymsbah2@gmail.com>
2025-08-27 21:52:50 +0300

    solve deploy problem

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/click-and-cook/", // <-- your GitHub repo name
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : "/click-and-cook/",
   plugins: [react()],
-});
+}));
